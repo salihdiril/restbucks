5eb87378c031fc34b42004fba79e8cf54ebca057
salih.diril <salih.diril@i2i-systems.com>
2025-03-31 07:41:33 +0300

    Spring boot dependencies are added and Main class annotated with SpringBootApplication annotation

--- src/main/java/com/salih/restbucks/Main.java
+++ src/main/java/com/salih/restbucks/Main.java
@@ -1,5 +1,8 @@
 package com.salih.restbucks;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 	public static void main(String[] args) {
 		// initial state
