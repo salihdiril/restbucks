61bd2f793752126d4fca34be41fd96d88c2d5a90
salih.diril <salih.diril@i2i-systems.com>
2025-04-03 08:41:46 +0300

    parse method split into multiple low-level helper methods since it doesn't adhere to SRP, SoC, Abstraction Layering, Testability principles.

--- restbucks-server/src/main/java/com/salih/restbucks/server/web/util/OrderUriParser.java
+++ restbucks-server/src/main/java/com/salih/restbucks/server/web/util/OrderUriParser.java
@@ -21,23 +21,40 @@ public enum OrderUriParser implements Loggable {
 
 	public static Order parse(Map<String, String> params, ProductCatalog productCatalog) {
 		StaticLogger.logEnter(OrderUriParser.class);
-		String productParam = params.get("product");
-		String quantityParam = params.getOrDefault("quantity", "1");
+		String productParam = extractProductParam(params);
+		int quantityParam = extractQuantityParam(params);
 
 		if (productParam == null) {
 			return null;
 		}
 
-		MenuItem menuItem = MenuItem.valueOf(productParam.toUpperCase());
+		MenuItem menuItem = toMenuItem(productParam);
 		Product product = productCatalog.findByName(menuItem.name());
 
-		List<Attribute> attributes = params.entrySet().stream() //
+		List<Attribute> attributes = extractAttributeParams(params);
+
+		Item item = new Item(product, quantityParam, attributes);
+		StaticLogger.logExit(OrderUriParser.class);
+		return new Order(UUID.randomUUID().toString(), List.of(item), ConsumeLocation.TAKE_AWAY, OrderStatus.PENDING, 0.0, "USD");
+	}
+
+	private static String extractProductParam(Map<String, String> params) {
+		return params.get("product");
+	}
+
+	private static int extractQuantityParam(Map<String, String> params) {
+		String quantityParam = params.getOrDefault("quantity", "1");
+		return Integer.parseInt(quantityParam);
+	}
+
+	private static List<Attribute> extractAttributeParams(Map<String, String> params) {
+		return params.entrySet().stream() //
 				.filter(e -> !e.getKey().equals("product") && !e.getKey().equals("quantity")) //
 				.map(e -> new Attribute(PropertyKey.valueOf(e.getKey().toUpperCase()), e.getValue())) //
 				.toList();
+	}
 
-		Item item = new Item(product, Integer.parseInt(quantityParam), attributes);
-		StaticLogger.logExit(OrderUriParser.class);
-		return new Order(UUID.randomUUID().toString(), List.of(item), ConsumeLocation.TAKE_AWAY, OrderStatus.PENDING, 0.0, "USD");
+	private static MenuItem toMenuItem(String name) {
+		return MenuItem.valueOf(name.toUpperCase());
 	}
 }
