7fd9b7a555902e3ea25de5ea2bd8873e7299239b
salih.diril <salih.diril@i2i-systems.com>
2025-04-06 12:23:19 +0300

    Logs are added to the mapper classes and log4j2 file deleted from the common module ince it is redundant

--- restbucks-server/src/main/java/com/salih/restbucks/server/web/pox/mapper/OrderConfirmationMapper.java
+++ restbucks-server/src/main/java/com/salih/restbucks/server/web/pox/mapper/OrderConfirmationMapper.java
@@ -2,15 +2,21 @@ package com.salih.restbucks.server.web.pox.mapper;
 
 import java.util.List;
 
+import com.salih.restbucks.common.log.Loggable;
+import com.salih.restbucks.common.log.StaticLogger;
 import com.salih.restbucks.server.domain.Order;
 import com.salih.restbucks.server.web.pox.xmlmodel.OrderConfirmation;
 import com.salih.restbucks.server.web.pox.xmlmodel.OrderStatus;
 
-public class OrderConfirmationMapper {
+public class OrderConfirmationMapper implements Loggable {
 	public static OrderConfirmation map(Order order) {
+		StaticLogger.logEnter(OrderConfirmationMapper.class);
+		StaticLogger.logger(OrderConfirmationMapper.class).atDebug().log("Mapping orderId={}, itemCount={}", order.getOrderId(), order.getItems().size());
+
 		List<String> items = order.getItems().stream() //
 				.map(item -> item.product().name()) //
 				.toList();
+		StaticLogger.logger(OrderConfirmationMapper.class).atTrace().log("Extracted item names: {}", items);
 
 		OrderConfirmation confirmation = new OrderConfirmation();
 		confirmation.setOrderId(order.getOrderId());
@@ -18,7 +24,10 @@ public class OrderConfirmationMapper {
 		confirmation.setCost(order.getCost());
 		confirmation.setCurrency(order.getCurrency());
 		confirmation.setStatus(OrderStatus.valueOf(order.getStatus().name()));
+		StaticLogger.logger(OrderConfirmationMapper.class).atDebug().log("Mapped OrderConfirmation: {}", confirmation);
 
+
+		StaticLogger.logExit(OrderConfirmationMapper.class);
 		return confirmation;
 	}
 }
--- restbucks-server/src/main/java/com/salih/restbucks/server/web/pox/mapper/XmlOrderMapper.java
+++ restbucks-server/src/main/java/com/salih/restbucks/server/web/pox/mapper/XmlOrderMapper.java
@@ -3,6 +3,8 @@ package com.salih.restbucks.server.web.pox.mapper;
 import java.util.List;
 import java.util.UUID;
 
+import com.salih.restbucks.common.log.Loggable;
+import com.salih.restbucks.common.log.StaticLogger;
 import com.salih.restbucks.server.domain.Attribute;
 import com.salih.restbucks.server.domain.ConsumeLocation;
 import com.salih.restbucks.server.domain.Item;
@@ -12,13 +14,17 @@ import com.salih.restbucks.server.domain.ProductType;
 import com.salih.restbucks.server.domain.PropertyKey;
 import com.salih.restbucks.server.web.pox.xmlmodel.Product;
 
-public class XmlOrderMapper {
+public class XmlOrderMapper implements Loggable {
 
 	public static Order map(com.salih.restbucks.server.web.pox.xmlmodel.Order xmlOrder) {
+		StaticLogger.logEnter(XmlOrderMapper.class);
+
 		List<Item> domainItems = xmlOrder.getItems().getItem().stream() //
 				.map(XmlOrderMapper::mapItem) //
 				.toList();
+		StaticLogger.logger(XmlOrderMapper.class).atTrace().log("Extracted item names: {}", domainItems);
 
+		StaticLogger.logExit(XmlOrderMapper.class);
 		return new Order(UUID.randomUUID().toString(), //
 				domainItems, //
 				ConsumeLocation.valueOf(xmlOrder.getLocation().value()), //
@@ -28,17 +34,22 @@ public class XmlOrderMapper {
 	}
 
 	private static Item mapItem(com.salih.restbucks.server.web.pox.xmlmodel.Item xmlItem) {
+		StaticLogger.logEnter(XmlOrderMapper.class);
 		Product xmlProduct = xmlItem.getProduct();
+		StaticLogger.logger(XmlOrderMapper.class).atDebug().log("Mapping xmlItem for product: {}, quantity: {}", xmlProduct.getName(), xmlItem.getQuantity());
 
 		List<Attribute> attributes = xmlProduct.getAttribute().stream() //
 				.map(attribute -> new Attribute(PropertyKey.valueOf(attribute.getName().name()), attribute.getValue())) //
 				.toList();
+		StaticLogger.logger(XmlOrderMapper.class).atTrace().log("Mapped attributes: {}", attributes);
 
 		com.salih.restbucks.server.domain.Product product = new com.salih.restbucks.server.domain.Product( //
 				xmlProduct.getName(), //
 				ProductType.valueOf(xmlProduct.getType().name()), //
 				attributes.stream().map(Attribute::propertyKey).toList());
+		StaticLogger.logger(XmlOrderMapper.class).atTrace().log("Created domain product: {}", product);
 
+		StaticLogger.logExit(XmlOrderMapper.class);
 		return new Item(product, xmlItem.getQuantity(), attributes);
 	}
 }
